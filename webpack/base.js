e8f9ab6699853cb1378a8d00aa1b46aa7302cb7e
sam mok <sammok2003@hotmail.com>
2017-04-28 17:11:34 +0800

    Update base.js
    fixed publicPath problem

--- webpack/base.js
+++ webpack/base.js
@@ -11,7 +11,7 @@ module.exports = function () {
 
             path: path.resolve(__dirname, '../dist'),
 
-            publicPath: ''
+            publicPath: '/'
             // necessary for HMR to know where to load the hot update chunks
         },
         module: {
